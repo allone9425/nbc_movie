f5f25155ebde093418cccef7265282066fcff616
이다원 <143374855+allone9425@users.noreply.github.com>
2023-10-20 16:49:29 +0900

    UI 개선

--- script_swiper.js
+++ script_swiper.js
@@ -12,18 +12,23 @@
 //   },
 // });
 
- var swiper = new Swiper(".mySwiper", {
-   slidesPerView: 2,
-   spaceBetween: 30,
-   loop: true,      
-//   effect: "fade",
-   pagination: {
-     el: ".swiper-pagination",
-     clickable: true,
-     type: "progressbar",
-   },
-   navigation: {
-    nextEl: ".swiper-button-next",
-     prevEl: ".swiper-button-prev",
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  loop: true,
+  //   effect: "fade",
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
   },
- });
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
